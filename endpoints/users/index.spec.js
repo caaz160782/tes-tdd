137d9f2a5b86b327fe6978b957944d75b6c60fef
Carlos Aguirre <carlos.aguirre@it-seekers.com>
2023-09-06 19:46:54 -0600

    test

---
+++ endpoints/users/index.spec.js
@@ -0,0 +1,22 @@
+const handlers = require('./index')
+
+describe('Endpoints',()=>{
+    describe('users', ()=>{
+        describe('get',()=>{
+            it('return to user json',async ()=>{
+                const axios ={
+                    get: jest.fn().mockResolvedValue({data:1})
+                }
+                const res = {
+                    status :jest.fn().mockReturnThis(),
+                    send   :jest.fn()
+                }
+                //console.log(axios.get)
+                await handlers({axios}).get({},res);
+                expect(res.status.mock.calls).toEqual([[200]])
+                expect(res.send.mock.calls).toEqual([[1]])
+            })
+            
+        })
+    })
+})
